b64fa709675dd8aa5870c54a2136364de8991476
goingenginia <goingenginia-programing@yahoo.co.jp>
2019-08-17 18:15:39 +0900

    テンプレート文字列を修正しました

--- main.js
+++ main.js
@@ -12,10 +12,10 @@
 
 for (let i = 1; i < 100; i++) {
     if (i % 3 === 0 && i % 5 === 0) {
-        console.log("FizzBuzz" + `${i}`);
+        console.log("FizzBuzz" + i);
     } else if (i % 3 === 0) {
-        console.log("Fizz" + `${i}`);
+        console.log("Fizz" + i);
     } else if (i % 5 === 0) {
-        console.log("Buzz" + `${i}`);
+        console.log("Buzz" + i);
     }
 }
